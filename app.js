3b0b44561f21a92586e9e6e2f4b3172f012ee81f
pardue95 <cwpardue@gmail.com>
2021-11-09 14:39:08 -0600

    added validation to questions

--- app.js
+++ app.js
@@ -11,16 +11,32 @@ const inquirer = require('inquirer');
 // });
 const promptUser = () => {
     return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-      },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
-      },
+        message: 'Enter your GitHub Username',
+        validate: githubUserName => {
+            if (githubUserName) {
+              return true;
+            } else {
+              console.log('Please enter Github user name!');
+              return false;
+            }
+          }
+        },
       {
         type: 'input',
         name: 'about',
@@ -42,13 +58,29 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
-      },
+        message: 'What is the name of your project?',
+        validate: projectName => {
+            if (projectName) {
+              return true;
+            } else {
+              console.log('Please enter Github user name!');
+              return false;
+            }
+          }
+        },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
-      },
+        message: 'Provide a description of the project (Required)',
+        validate: projectDescription => {
+            if (projectDescription) {
+              return true;
+            } else {
+              console.log('Please enter Github user name!');
+              return false;
+            }
+          }
+        },
       {
         type: 'checkbox',
         name: 'languages',
@@ -58,8 +90,16 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
-      },
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: githubLink => {
+            if (githubLink) {
+              return true;
+            } else {
+              console.log('Please enter Github user name!');
+              return false;
+            }
+          }
+        },
       {
         type: 'confirm',
         name: 'feature',
